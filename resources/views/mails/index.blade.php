36f61089def774c5a7a7bae1e2449d6cae9d17ba
Eliza Ahmed <elizarafee@gmail.com>
2021-07-05 00:11:51 +0100

    alert style added

--- resources/views/mails/index.blade.php
+++ resources/views/mails/index.blade.php
@@ -3,26 +3,34 @@
 @section('content')
 
 <div class="mail-log">
-    <h1>Mail Log</h1>
-    <table class="table">
-  <thead>
-    <tr>
-      <th>#</th>
-      <th>Sender</th>
-      <th>Receiver</th>
-      <th>Receiver Email Adddress</th>
-      <th>Sent At</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>1</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-    </tr>
-  </tbody>
-</table>
+  <h1>Mail Log</h1>
+
+  @if(session('success'))
+  <div class="alert alert-success">
+    {{ session('success') }}
+  </div>
+  @endif
+
+
+  <table class="table">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Sender</th>
+        <th>Receiver</th>
+        <th>Receiver Email Adddress</th>
+        <th>Sent At</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+        <td>@mdo</td>
+      </tr>
+    </tbody>
+  </table>
 </div>
 @endsection
