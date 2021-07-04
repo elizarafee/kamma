d58589fe1064043ad32e3112aea6bbd5c6ff529b
Eliza Ahmed <elizarafee@gmail.com>
2021-07-05 00:05:38 +0100

    mail log style added

--- resources/views/mails/index.blade.php
+++ resources/views/mails/index.blade.php
@@ -4,7 +4,25 @@
 
 <div class="mail-log">
     <h1>Mail Log</h1>
+    <table class="table">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>Sender</th>
+      <th>Receiver</th>
+      <th>Receiver Email Adddress</th>
+      <th>Sent At</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+    </tr>
+  </tbody>
+</table>
 </div>
-
-
 @endsection
