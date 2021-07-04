bb1c17a77a4ad3d60fe264618b3a6029a52f7969
Eliza Ahmed <elizarafee@gmail.com>
2021-07-04 22:08:07 +0100

    styling main page content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,44 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Home</h1>
+<div class="content">
+    <h1 class="title">Kamma</h1>
+
+    <p class="sub-title">Lorem ipsum dolor sit amet consectetur, adipisicing elit.Accusamus, beatae? Lorem 
+        ipsum dolor sit amet consectetur, adipisicing elit. Perferendis, quam!</p>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium laboriosam consequatur maiores eum
+        impedit deserunt, ipsa cumque, dolores vitae suscipit dignissimos reprehenderit fugiat, error
+        sed illum ab tempora vero! Inventore. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Eos incidunt in autem voluptas maxime corporis et.</p>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium laboriosam consequatur maiores eum
+        impedit deserunt, ipsa cumque, dolores vitae suscipit dignissimos reprehenderit fugiat, error
+        sed illum ab tempora vero! Inventore. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Eos incidunt in autem voluptas maxime corporis et consequuntur ipsum ab laborum tempore, veniam ea
+        asperiores harum molestiae voluptatum dolorem laudantium neque similique nihil quidem ipsam expedita.
+        Minima itaque debitis vero nemo! Reprehenderit atque itaque error incidunt consectetur molestiae
+        voluptatum quos recusandae! To find out more<a href="#" class="link"> click here</a></p>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium laboriosam consequatur maiores eum
+        impedit deserunt, ipsa cumque, dolores vitae suscipit dignissimos reprehenderit fugiat, error
+        sed illum ab tempora vero! Inventore. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Eos incidunt in autem voluptas maxime corporis et consequuntur ipsum ab laborum tempore, veniam ea
+        asperiores harum molestiae voluptatum dolorem laudantium neque similique nihil quidem ipsam expedita.
+        Minima itaque debitis vero nemo!</p>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium laboriosam consequatur maiores eum
+        impedit deserunt, ipsa cumque, dolores vitae suscipit dignissimos reprehenderit fugiat, error
+        sed illum ab tempora vero! Inventore. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Eos incidunt in autem voluptas maxime corporis et consequuntur ipsum ab laborum tempore, veniam ea
+        asperiores harum molestiae voluptatum dolorem laudantium neque similique nihil quidem ipsam expedita.
+        Minima itaque debitis vero nemo! Reprehenderit atque itaque error incidunt consectetur molestiae
+        voluptatum quos recusandae!</p>
+
+    <a href="#" class="read-more-link">Read more about how we work
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z" />
+        </svg>
+    </a>
+</div>
 @endsection
