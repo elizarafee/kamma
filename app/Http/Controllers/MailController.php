680dd0db13d9f77e94351a8a6a65339d65e06346
Eliza Ahmed <elizarafee@gmail.com>
2021-07-04 22:51:02 +0100

    mail controller created and methods added

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MailController extends Controller
+{
+    /**
+     * Display a listing of emails.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for sending email.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly sent email into storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+}
