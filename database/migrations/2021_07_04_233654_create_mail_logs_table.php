8124dbb89aa6689b0bd0a04970accc073f9c8ce1
Eliza Ahmed <elizarafee@gmail.com>
2021-07-05 00:41:43 +0100

    model and migration added for maillog

---
+++ database/migrations/2021_07_04_233654_create_mail_logs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMailLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mail_logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('sender_name');
+            $table->string('receiver_name');
+            $table->string('receiver_email');
+            $table->text('email_message')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mail_logs');
+    }
+}
