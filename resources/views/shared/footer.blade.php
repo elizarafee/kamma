c8805b048357cfdeb5485264c51f81fa6e77e519
Eliza Ahmed <elizarafee@gmail.com>
2021-07-04 21:52:50 +0100

    footer styling added

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -1,9 +1,9 @@
 <footer>
     <div>
-        <p class="copyright">@Kamma 2020</p>
         <ul class="footer-list">
-            <li><a href="">Privacy policy</a></li>
-            <li><a href="">Term & conditions</a></li>
+            <li>&copy; Kamma 2020</li>
+            <li><a href="">Privacy Policy</a></li>
+            <li><a href="">Terms and Conditions</a></li>
             <li><a href="">Contact</a></li>
         </ul>
     </div>
